a052a5478b1a58f9087ddfc07891802593537bbb
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-03-13 02:00:01 +0100

    Updated

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+last_digit1 = number % -10
+if number > 0:
+    if last_digit > 5:
+        print(f"Last digit of {number} is", last_digit, "and is greater\
+ than 5")
+    elif last_digit == 0:
+        print(f"Last digit of {number} is", last_digit, "and is 0")
+    elif (last_digit < 6) and (last_digit != 0):
+        print(f"Last digit of {number} is", last_digit, "and is less than 6\
+ and not 0")
+elif number < 0:
+    if (last_digit1 < 6) and (last_digit1 != 0):
+        print(f"Last digit of {number} is", last_digit1, "and is less than 6\
+ and not 0")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(0, 99):
+    print(i, "= 0x{:x}".format(i))
