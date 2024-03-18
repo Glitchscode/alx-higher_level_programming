e9da5b1f0ad5f3f1f02ce748fc8a48a0dcf4bb56
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-03-18 10:23:14 +0100

    first task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Print the sum of 1 and 2."""
-"""def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))"""
 def print_list_integer(my_list=[]):
-    a = [print("{}".format(my_list[i])) for i in range(len(my_list))]
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
