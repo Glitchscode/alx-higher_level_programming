9beb47c31f5202b7bbe99f1c45ab46595aaf8ca8
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-03-18 17:08:01 +0100

    fourth task

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print_reversed_list_integer(my_list)
