c334f5527d0680cbf9a2ed517d45ac33cf3ceeda
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-03-18 21:56:39 +0100

    seventh task

---
+++ 0x03-python-data_structures/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+print_matrix_integer(mat)
+print("--")
+print_matrix_integer()
