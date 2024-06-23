a75cd790b5eecfb174ca4c1f1f790e3bea2b6c13
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-06-23 22:03:08 +0100

    Some more task completed

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    our_user = session.query(State).filter_by(id=1).first()
+    if our_user is None:
+        print("Nothing")
+    else:
+        print(our_user.id, our_user.name, sep=": ")
